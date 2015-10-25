70e88bd797f9b9853edc1e4fd13108a94df9eb9c
cat_in_136 <cat.in.136+github@gmail.com>
2015-10-25 20:03:00 +0900

    initial commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_scheduling_poll do
+  name 'Scheduling Poll plugin'
+  author '@cat_in_136'
+  description 'provide simple polls to scheduling appointments'
+  version '0.0.0'
+  url 'https://github.com/cat-in-136/redmine_scheduling_poll'
+  author_url 'https://github.com/cat-in-136/'
+end
