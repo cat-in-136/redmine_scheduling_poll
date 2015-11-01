e9eb24ba91d915b65c26a429453257711efff210
cat_in_136 <cat.in.136+github@gmail.com>
2015-11-01 18:33:20 +0900

    Enhance UI and apply redmine style

--- app/helpers/scheduling_polls_helper.rb
+++ app/helpers/scheduling_polls_helper.rb
@@ -29,4 +29,27 @@ module SchedulingPollsHelper
     link_to_function name, js_func
   end
 
+  def render_date_related_parameter_of_issue(issue)
+    s = ''
+    s << '<h3>' << link_to(issue, issue_path(issue)) << '</h3>'
+    s << '<div>'
+    unless issue.disabled_core_fields.include?('start_date')
+      s << '<p><strong>' << h(l(:field_start_date)) << '</strong></p>'
+      s << content_tag(:span, format_date(issue.start_date), :class => 'start-date')
+    end
+    unless issue.disabled_core_fields.include?('due_date')
+      s << '<p><strong>' << h(l(:field_due_date)) << '</strong></p>'
+      s << content_tag(:span, format_date(issue.due_date), :class => 'due-date')
+    end
+    # show date-related custom field values
+    issue.visible_custom_field_values.each do |value|
+      if value.custom_field.field_format == "date"
+        s << '<p><strong>' << h(CustomFieldsController.helpers.custom_field_name_tag(value.custom_field)) << '</strong></p>'
+        s << content_tag(:span, CustomFieldsController.helpers.show_value(value))
+      end
+    end
+    s << '</div>'
+    s.html_safe
+  end
+
 end
