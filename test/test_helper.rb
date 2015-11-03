b911c44c02e5359cf5e7302d99a96bec6d5312ff
cat_in_136 <cat.in.136+github@gmail.com>
2015-11-04 01:02:08 +0900

    remove unused code in test_helper.rb

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,18 +1,2 @@
 # Load the Redmine helper
 require File.expand_path(File.dirname(__FILE__) + '/../../../test/test_helper')
-
-# https://www.redmine.org/boards/3/topics/35164
-module Redmine
-  module PluginFixturesLoader
-    def self.included(base)
-      base.class_eval do
-        def self.plugin_fixtures(*symbols)
-          ActiveRecord::Fixtures.create_fixtures(File.dirname(__FILE__) + '/fixtures/', symbols)
-        end
-      end
-    end
-  end
-end
-unless ActionController::TestCase.included_modules.include?(Redmine::PluginFixturesLoader)
-  ActionController::TestCase.send :include, Redmine::PluginFixturesLoader
-end
