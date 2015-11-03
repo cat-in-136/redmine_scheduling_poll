8c5a2d8d9d843d55c73cebb7c13bceb5a5ff88b7
cat_in_136 <cat.in.136+github@gmail.com>
2015-11-03 10:59:14 +0900

    add travis-ci test scripts

---
+++ bin/travisci_before_install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# inspired by https://gist.github.com/pinzolo/7599006
+
+#REDMINE_VERSION="2.4.0"
+PLUGIN_NAME=redmine_scheduling_poll
+
+# Get & deploy Redmine
+wget http://www.redmine.org/releases/redmine-${REDMINE_VERSION}.tar.gz
+tar zxf redmine-${REDMINE_VERSION}.tar.gz
+
+# Clone plugin files to plugin directory
+git clone . redmine-${REDMINE_VERSION}/plugins/${PLUGIN_NAME}
+
+# Create necessary files
+cat > redmine-${REDMINE_VERSION}/config/database.yml <<_EOS_
+test:
+  adapter: sqlite3
+  database: db/redmine_test.db
+_EOS_
