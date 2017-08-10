75ba02ae050517a1e9a8e568627e21bd41f233f1
cat_in_136 <cat.in.136+github@gmail.com>
2017-08-10 20:35:34 +0900

    for travis-ci, enable cache

--- bin/travisci_before_install.sh
+++ bin/travisci_before_install.sh
@@ -4,6 +4,11 @@
 #REDMINE_VERSION="2.4.0"
 PLUGIN_NAME=redmine_scheduling_poll
 
+# Shelve the cache if exists
+if [ -f redmine/vendor/bundle ]; then
+  mv redmine/vendor /tmp/vendor
+fi
+
 # Shelve the plugin files to a temporary directory
 cp -pr . /tmp/${PLUGIN_NAME}
 
@@ -11,6 +16,11 @@ cp -pr . /tmp/${PLUGIN_NAME}
 wget https://www.redmine.org/releases/redmine-${REDMINE_VERSION}.tar.gz
 tar zxf redmine-${REDMINE_VERSION}.tar.gz
 rsync -a redmine-${REDMINE_VERSION}/ redmine/
+
+# Restore the cache if exists
+if [ -f /tmp/vendor ]; then
+  mv /tmp/vendor redmine/vendor
+fi
 
 # Copy the plugin files to plugin directory
 cp -pr /tmp/${PLUGIN_NAME} redmine/plugins/${PLUGIN_NAME}
