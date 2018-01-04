f7f94036af2578061af127e1927ef885e9bb856b
cat_in_136 <cat.in.136+github@gmail.com>
2018-01-04 20:42:59 +0900

    version v3.0.0

--- init.rb
+++ init.rb
@@ -6,7 +6,7 @@ Redmine::Plugin.register :redmine_scheduling_poll do
   name 'Scheduling Poll plugin'
   author '@cat_in_136'
   description 'provide simple polls to scheduling appointments'
-  version '2.3.0'
+  version '3.0.0'
   url 'https://github.com/cat-in-136/redmine_scheduling_poll'
   author_url 'https://github.com/cat-in-136/'
 
