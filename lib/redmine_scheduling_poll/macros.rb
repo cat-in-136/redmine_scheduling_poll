89937581e6f64440cc4607500911b513a0d11813
Tobias Matthaiou <developer@tobimat.eu>
2018-09-05 00:06:21 +0200

    A small GUI proposal.

--- lib/redmine_scheduling_poll/macros.rb
+++ lib/redmine_scheduling_poll/macros.rb
@@ -4,6 +4,6 @@ Redmine::WikiFormatting::Macros.register do
   macro :scheduling_poll do |obj, args|
     id = args.first
     scheduling_poll = SchedulingPoll.find(id)
-    link_to("{{scheduling_poll(#{id})}}", scheduling_poll_url(scheduling_poll))
+    link_to( l(:label_link_scheduling_poll, num: id), scheduling_poll_url(scheduling_poll))
   end
 end
