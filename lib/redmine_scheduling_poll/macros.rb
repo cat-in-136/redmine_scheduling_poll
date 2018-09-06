e0dcb4bca63b2dd21f11d45c8123639c3ebb5515
cat_in_136 <cat.in.136+github@gmail.com>
2018-09-07 00:19:12 +0900

    add description to scheduling_poll macro

--- lib/redmine_scheduling_poll/macros.rb
+++ lib/redmine_scheduling_poll/macros.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Redmine::WikiFormatting::Macros.register do
+  desc "Inserts a link to a scheduling poll (/scheduling_polls/:id/show). Example:\n\n" +
+      "{{scheduling_poll(1)}} -- link to /scheduling_polls/1/show"
   macro :scheduling_poll do |obj, args|
     id = args.first
     scheduling_poll = SchedulingPoll.find(id)
