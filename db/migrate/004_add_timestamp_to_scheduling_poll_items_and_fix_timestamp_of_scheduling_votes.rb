dea740ec16e7672e7b9e39819e4325a095951f00
cat_in_136 <cat.in.136+github@gmail.com>
2018-09-09 14:31:38 +0900

    manually specify `null: true` to t.timestamps on the migration script
    In Rails 5, this behavior will change to `null: false`. This change prevents the behavior of the existing migrations from changing.

--- db/migrate/004_add_timestamp_to_scheduling_poll_items_and_fix_timestamp_of_scheduling_votes.rb
+++ db/migrate/004_add_timestamp_to_scheduling_poll_items_and_fix_timestamp_of_scheduling_votes.rb
@@ -1,7 +1,7 @@
 class AddTimestampToSchedulingPollItemsAndFixTimestampOfSchedulingVotes < ActiveRecord::Migration
   def change
     change_table :scheduling_poll_items do |t|
-      t.timestamps
+      t.timestamps :null => true
     end
 
     rename_column :scheduling_votes, :create_at, :created_at
