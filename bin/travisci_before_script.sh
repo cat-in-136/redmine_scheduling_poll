314933e7ecc44a38f58b5868f1b5afd8da4d081a
cat_in_136 <cat.in.136+github@gmail.com>
2019-06-02 16:24:10 +0900

    update .travis.yml to latest ruby/redmine versions

--- bin/travisci_before_script.sh
+++ bin/travisci_before_script.sh
@@ -7,6 +7,12 @@ export RAILS_ENV=test
 #REDMINE_VERSION="2.4.0"
 PLUGIN_NAME=redmine_scheduling_poll
 
+# https://www.redmine.org/issues/30353
+if [ "v${REDMINE_VERSION:0:2}" = "v3." ]; then
+  rvm @global do gem uninstall bundler -x -v ">= 2.0" 
+  gem install bundler -v "< 2.0" 
+fi
+
 cd redmine
 
 bundle install --path=${BUNDLE_PATH:-vendor/bundle}
